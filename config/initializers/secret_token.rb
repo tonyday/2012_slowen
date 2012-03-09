4f096b98ea9459f43e2c1ac2118ba87f7bd9a8d8
Tony Day <tony@lazylabs.com>
2012-03-09 22:53:08 +1300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slowen::Application.config.secret_token = 'f9582414df71bfa2a5566f0638f83d111de0c260654823c1fad123c4a55ea460aad1aaf7bcacb7136d8a7f10072819d533d01fd356c9bf2de453cdba4e7c0dfc'

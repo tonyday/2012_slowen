2ec7c79d946c50e9458899195381612745dd133c
Tony Day <tony@lazylabs.com>
2012-03-11 09:37:46 +1300

    Add sign out to Welcome page.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,6 +13,8 @@ describe "Authentication" do
   describe 'signin' do
     before { visit signin_path }
 
+    it { should_not have_link('Sign out') }
+
     describe 'with invalid information' do
       before { click_button 'Sign in' }
 
@@ -25,17 +27,18 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in(user) }
       it { should have_content('Welcome to Slowen') }
+      it { should have_link('Sign out', href: signout_path) }
 
       #it { should have_selector('title', test: user.name) }
       #it { should have_link('Profile', href: user_path(user)) }
       #it { should have_link('Settings', href: edit_user_path(user)) }
-      #it { should have_link('Sign out', href: signout_path) }
       #it { should_not have_link('Sign in', href: signin_path) }
-      #
-      #describe 'followed by signout' do
-      #  before { click_link 'Sign out' }
-      #  it { should have_link('Sign in') }
-      #end
+
+      describe 'followed by signout' do
+        before { click_link 'Sign out' }
+        it { should have_selector('h1', text: 'Sign in') }
+        it { should have_selector('title', text: 'Sign in') }
+      end
 
     end
 

8ef886ce93bcc022d6965c8ecf721990e0debddd
Tony Day <tony@lazylabs.com>
2012-03-11 21:55:17 +1300

    Refactor layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
 
   def display_flash
-    flash.each do |key, value|
-      content_tag(:div, value, class: "flash #{key}")
+    flash.inject('') do |result, arr|
+      result += content_tag(:div, arr[1], class: "flash #{arr[0]}")
+    end.html_safe
+  end
+
+  def full_title(page_title)
+    base_title = "Slowen"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
     end
   end
 
